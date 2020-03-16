47b8d4aa59c60844d0fdb453be753de802b01ba9
ropay <Dimononly@gmail.com>
2020-03-16 17:58:23 +0200

    UI changes: add user avatar to comment item

--- src/main/resources/static/js/ui/components/comments/CommentItem.tsx
+++ src/main/resources/static/js/ui/components/comments/CommentItem.tsx
@@ -1,17 +1,41 @@
 import * as React from 'react';
 import { IComment } from 'model/IComment';
+import { Grid, Avatar, Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 interface ICommentItemProps {
-    comment: IComment;
+  comment: IComment;
 }
 
-const CommentItem = ({ comment: { text } }: ICommentItemProps) => {
-    return (
-        <div>
-            { text }
-        </div>
-    );
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    small: {
+      width: theme.spacing(3),
+      height: theme.spacing(3),
+    },
+  }),
+);
 
+const CommentItem = ({ comment }: ICommentItemProps) => {
+  const classes = useStyles({});
+  return (
+    <Grid
+      container
+      direction="row"
+      justify="flex-start"
+      alignItems="center"
+      spacing={ 1 }
+    >
+      <Grid item>
+        <Avatar alt="Remy Sharp" src={ comment.author.userpic } className={ classes.small } />
+      </Grid>
+      <Grid item>
+        <Typography>
+          { comment.text }
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default CommentItem;
